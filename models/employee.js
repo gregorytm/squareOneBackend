dada249a104963149c765237eb788cd1946c956b
Greg Marsh <gregorytm@users.noreply.github.com>
2022-05-11 12:31:33 -0700

    removes unused import

--- models/employee.js
+++ models/employee.js
@@ -2,7 +2,7 @@
 
 const db = require("../db");
 const bcrypt = require("bcrypt");
-const { sqlForPartialUpdate } = require("../helperFunctions/sql");
+
 const {
   NotFoundError,
   BadRequestError,
