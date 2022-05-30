cf3646967e61e77c15c5b6d9e76fa224b4cc732c
Greg Marsh <gregorytm@users.noreply.github.com>
2022-05-30 13:38:08 -0700

    adds todo

--- helperFunctions/isDefinedNumber.js
+++ helperFunctions/isDefinedNumber.js
@@ -1,8 +1,9 @@
-function isDefinedNumber(temp, RH, MC, day) {
-  const inputs = [temp, RH, MC, day];
-  for (const reading of inputs) {
-    reading && reading !== 0;
-  }
-}
+//TODO: remove this function after testing
+// function isDefinedNumber(temp, RH, MC, day) {
+//   const inputs = [temp, RH, MC, day];
+//   for (const reading of inputs) {
+//     reading && reading !== 0;
+//   }
+// }
 
-module.exports = { isDefinedNumber };
+// module.exports = { isDefinedNumber };
