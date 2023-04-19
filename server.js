837e28f8d1f28509f90c274f8f6ec2467a06d6e6
Greg Marsh <gregorytm@users.noreply.github.com>
2023-04-19 14:33:47 -0700

    login fix

--- server.js
+++ server.js
@@ -3,5 +3,4 @@ const app = require("./app");
 const port = process.env.PORT || 3001;
 
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`));
-``;
+app.listen(port, () => console.log(`app listening on port ${port}`));
